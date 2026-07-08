93a00acda25a324f4e66ee4221f8b2e7d4a92294
Ebad Mirza <ebadmirza.2005@gmail.com>
2026-07-08 16:14:16 +0500

    Remove Card From Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,33 +21,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 20,),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    TextWidget(text: "Zubair Ahmed", textSize: 18,),
-                    SizedBox(height: 10,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Align(
-                            alignment: Alignment.bottomLeft,
-                            child: TextWidget(text: "Campus: Johar Main Campus", textSize: 16,)),
-                        TextWidget(text: "No. of Vans:  25", textSize: 16,),
-                      ],
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: TextWidget(text: "Version 1.0.1", textSize: 10.0,),),
-                  ],
-                ),
-              ),
-            ),
-          ),
           SizedBox(height: MediaQuery.of(context).size.height * 0.2,),
           Padding(padding: EdgeInsets.all(10),
           child: Column(
